d542d6533091ac93c7ac50b743cdc5ea953eee54
Alex <wbyun1461@gmail.com>
2023-02-25 15:13:51 +0900

    회원가입 중복아이디 409 상태코드 반환

--- src/main/java/com/example/demo/controller/UserRegisterController.java
+++ src/main/java/com/example/demo/controller/UserRegisterController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import com.example.demo.domain.dto.UserJoinRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,11 +17,18 @@ public class UserRegisterController {
 
      private final UserService userService;
 
-     //회원 가입 API
      @PostMapping(value = "/register")
      public ResponseEntity<String> registerUser(@RequestBody UserJoinRequest dto){
           String result = userService.join(dto.getUserName(),dto.getPassword());
-          return result=="success" ? ResponseEntity.ok().body("회원가입이 성공 했습니다!") : ResponseEntity.ok().body("회원가입이 실패했습니다");
+
+          if(result=="success"){
+               return ResponseEntity.ok().body("회원가입이 성공 했습니다!");
+          }
+
+          else{
+               return new ResponseEntity<>("중복아이디여서 회원가입이 실패했습니다. 다른 아이디를 입력해주세요!",HttpStatus.CONFLICT);
+          }
+          
      }
 
 }
